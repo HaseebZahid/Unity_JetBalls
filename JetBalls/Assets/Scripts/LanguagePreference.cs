33dd5fc5b8ba89932b803512dc57211b92f14c19
HaseebZahid <haseebzahid413@gmail.com>
2021-08-05 20:55:10 +0500

    language module

---
+++ JetBalls/Assets/Scripts/LanguagePreference.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguagePreference : MonoBehaviour
+{
+    public const string prefKey = "LanguagePreferenceKey";
+
+    public static LanguagePreference instance;
+    private void Awake()
+    {
+        instance = this;
+
+        langValue = PlayerPrefs.GetInt(prefKey, 0);
+    }
+
+    int m_value;
+
+    public int langValue
+    {
+        get
+        {
+            return m_value;
+        }
+
+        set
+        {
+            m_value = value;
+            PlayerPrefs.SetInt(prefKey, m_value);
+        }
+    }
+
+}
