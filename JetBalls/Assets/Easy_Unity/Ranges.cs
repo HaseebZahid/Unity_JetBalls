61898a7a0034b820b621a2855f0fb30444c601f3
HaseebZahid <haseebzahid413@gmail.com>
2021-08-05 19:30:17 +0500

    Easy Unity tools

---
+++ JetBalls/Assets/Easy_Unity/Ranges.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+
+[Serializable]
+public struct FloatRange
+{
+    public float Min, Max;
+    public FloatRange(float min, float max) => (Min, Max) = (min, max);
+}
+
+[Serializable]
+public struct IntRange
+{
+    public int Min, Max;
+    public IntRange(int min, int max) => (Min, Max) = (min, max);
+}
+
+[Serializable]
+public struct ByteRange
+{
+    public byte Min, Max;
+    public ByteRange(byte min, byte max) => (Min, Max) = (min, max);
+}
